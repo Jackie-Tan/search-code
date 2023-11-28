18eb4ed68b28d37c160c5e06841adca023433051
Jackie <jwtan2010@gmail.com>
2023-11-28 18:13:57 +0800

    feat: add settings and delete unnecessary files

--- apply_patches.py
+++ apply_patches.py
@@ -5,24 +5,26 @@ import shutil
 import ast
 import sys
 import time
+from pathlib import Path
 
 import requests
 from git import Repo, exc, Git
 
-import config_loader
+import logging
+logger = logging.getLogger(__name__)
+import settings
+config = settings.get_config()
+token = config['github_token']
 
 # 1. Read CSV that has 2 columns: repo_patch_url, repo_base_url (done)
 # 2. Using the github_repo_url to clone the repo to local (done)
 # 3. download the patches to memory using github_patch_url (done)
 # 4. Iteratively checkout to all the tags and apply the patch to each checkout (done)
 # 5. If successful, create a new tag "***-secure". If not, ignore (done)
-# 5a. count the number of successful tag
+# 5a. count the number of successful tag (done)
 # 6. Push the repo to remote. Go back to step 2 and repeat the process
 
 patched_counter = 0
-
-config = config_loader.get_config()
-token = config['github_token']
 
 def check_repo_exists(library_name: str) -> bool:
     url = f"https://api.github.com/repos/scantist-ossops/{library_name}"
@@ -48,11 +50,7 @@ def read_csv_to_variable(file_path: str, start_row: int = 0, end_row: int = 100)
     data = []
     with open(file_path, mode='r', encoding='utf-8') as csvfile:
         csv_reader = csv.DictReader(csvfile)
-        for i, row in enumerate(csv_reader):
-            if start_row <= i < end_row:
-                data.append(row)
-            elif i >= end_row:
-                break
+        data = [row for i, row in enumerate(csv_reader) if start_row <= i < end_row]
     return data
 
 def get_org_library_name(url: str) -> tuple[str, str]:
@@ -65,15 +63,11 @@ def get_org_library_name(url: str) -> tuple[str, str]:
 def clone_to_local_repo(repo_base_url: str) -> Repo:
     org_name, library_name = get_org_library_name(repo_base_url)
     
-    # the file id_ed25519 is the ssh file that your command line environment
-    # uses to authenticate ssh git clone
-    git_ssh_identity_file = os.path.join(os.getcwd(), 'id_ed25519')
-    git_ssh_cmd = f'ssh -i {git_ssh_identity_file}'
-
     if not os.path.exists('./repo'):
         os.makedirs('./repo')
 
     repo_path: str = os.path.join(os.getcwd(), 'repo', f'{org_name}_{library_name}')
+    git_ssh_cmd = get_git_ssh_cmd()
     
     if not os.path.exists(repo_path) or not os.listdir(repo_path):
         try:
@@ -122,10 +116,11 @@ def apply_patches_to_repo(repo: Repo, patches: list):
             repo.git.checkout(tag, b=new_branch_name)
 
             # As long as there is a patch, it is considered a success
-            is_patched = False
-            for patch in patches:
-                if apply_one_patch(repo, patch):
-                    is_patched = True
+            is_patched = any(apply_one_patch(repo, patch) for patch in patches)
+            # is_patched = False
+            # for patch in patches:
+            #     if apply_one_patch(repo, patch):
+            #         is_patched = True
 
             # Once the tag is patched, there will be a new commit and a new tag named "<original tag>-secure"
             if not is_patched:
@@ -167,41 +162,70 @@ def parse_literal_to_list(string):
     except ValueError:
         return []
     
-def fork_repo_remote(repo_base_url):
+def fork_repo_remote(repo_base_url: str) -> str:
     # fork from given url to scantist-ossops
     # return a url pointing to scantist-ossops
     org_name, library_name = get_org_library_name(repo_base_url)
-    command = [
-        "curl", "-L",
-        "-X", "POST",
-        "-H", "Accept: application/vnd.github+json",
-        "-H", f"Authorization: Bearer {token}",
-        "-H", "X-GitHub-Api-Version: 2022-11-28",
-        f"https://api.github.com/repos/{org_name}/{library_name}/forks",
-        "-d", '{"organization":"scantist-ossops"}'
-    ]
+    api_url = f"https://api.github.com/repos/{org_name}/{library_name}/forks"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    data = {"organization": "scantist-ossops"}
+
     if check_repo_exists(library_name):
-        print(f"Repository: https://api.github.com/repos/{org_name}/{library_name}/forks already exists, skip fork")
+        print(f"Repository: {api_url} already exists, skip fork")
         return f"http://github.com/scantist-ossops/{library_name}"
+
     try:
-        subprocess.run(command, stdout=subprocess.PIPE, check=True)
+        response = requests.post(api_url, headers=headers, json=data)
+        response.raise_for_status()
         print(f"Forked: {org_name}/{library_name} to scantist-ossops/{library_name}")
         time.sleep(1)
         return f"http://github.com/scantist-ossops/{library_name}"
-    except subprocess.CalledProcessError as e:
-        print(f"Error forking repository: {e}")
+    except requests.HTTPError as e:
+        logger.error(f"HTTP error occurred while forking repository: {e}")
     except Exception as e:
-        print(f"An unexpected error occured: {e}")
+        logger.error(f"An unexpected error occurred: {e}")
+
     sys.exit(1)
+    # command = [
+    #     "curl", "-L",
+    #     "-X", "POST",
+    #     "-H", "Accept: application/vnd.github+json",
+    #     "-H", f"Authorization: Bearer {token}",
+    #     "-H", "X-GitHub-Api-Version: 2022-11-28",
+    #     f"https://api.github.com/repos/{org_name}/{library_name}/forks",
+    #     "-d", '{"organization":"scantist-ossops"}'
+    # ]
+    # if check_repo_exists(library_name):
+    #     print(f"Repository: https://api.github.com/repos/{org_name}/{library_name}/forks already exists, skip fork")
+    #     return f"http://github.com/scantist-ossops/{library_name}"
+    # try:
+    #     subprocess.run(command, stdout=subprocess.PIPE, check=True)
+    #     print(f"Forked: {org_name}/{library_name} to scantist-ossops/{library_name}")
+    #     time.sleep(1)
+    #     return f"http://github.com/scantist-ossops/{library_name}"
+    # except subprocess.CalledProcessError as e:
+    #     print(f"Error forking repository: {e}")
+    # except Exception as e:
+    #     print(f"An unexpected error occured: {e}")
+    # sys.exit(1)
+
+def get_git_ssh_cmd():
+    # the file id_ed25519 is the ssh file that your command line environment
+    # uses to authenticate ssh git clone
+    private_key_path = config['private_key_path']
+    file_path = Path(private_key_path)
+    git_ssh_identity_file = file_path.resolve()
+    return f'ssh -i {git_ssh_identity_file}'
 
 
 def main():
     global patched_counter
-    # Path to your CSV file
     csv_file_path = 'final_data_v3.csv'
-
-    # Read the CSV data into a Python variable
-    csv_data: list = read_csv_to_variable(csv_file_path, start_row=2, end_row=3)
+    csv_data: list = read_csv_to_variable(csv_file_path, start_row=0, end_row=2)
     
     for repo_patch_dict in csv_data:
         scantist_ossops_base_url = fork_repo_remote(repo_patch_dict['repo_base_url'])
@@ -209,12 +233,10 @@ def main():
         if not repo:
             continue
         repo_patch_urls = parse_literal_to_list(repo_patch_dict['repo_patch_urls'])
-        patches = []
-        for url in repo_patch_urls:
-            patches.append(download_patch(url))
+        patches = [download_patch(url) for url in repo_patch_urls]
         apply_patches_to_repo(repo, patches)
         # Once the patches are done, delete the local repo
-        # delete_local_repo(repo)
+        delete_local_repo(repo)
 
     print(f'Total versions patched: {patched_counter}')
 
---
+++ settings.py
@@ -0,0 +1,37 @@
+import json
+import logging
+import sys
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename='app.log',
+                    filemode='a')
+
+logger = logging.getLogger(__name__)
+
+global_config = None
+
+def load_config(crucial_keys) -> dict:
+    global global_config
+    try:
+        if global_config is None:
+            with open('config.json', 'r') as file:
+                global_config = json.load(file)
+
+            for key in crucial_keys:
+                if key not in global_config:
+                    raise ValueError(f"{key} not found in config.json, exiting now...")
+            return global_config
+    except FileNotFoundError:
+        logger.error("config.json file not found")
+        sys.exit(1)
+    except ValueError as e:
+        logger.error(str(e))
+        sys.exit(1)
+
+def get_config():
+    crucial_keys = ['github_token', 'private_key_path']
+    if global_config is None:
+        load_config(crucial_keys)
+    return global_config
